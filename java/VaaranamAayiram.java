c519cc4d6b6d7a3373fede6e36696bb7d2eb044d
ABINAYA M <132118365+Abinaya-M123@users.noreply.github.com>
2024-12-03 13:10:31 +0530

    Day 9

---
+++ java/VaaranamAayiram.java
@@ -0,0 +1,18 @@
+package Day8;
+import java.util.Scanner;
+public class VaaranamAayiram {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the status of Megana:");
+		String megana=sc.next();
+		String str=new String("Dead");
+		if(megana .equals(str)){
+			System.out.println("Suriya weds Ramya");
+		}
+		else {
+			System.out.println("Suriya weds Megana");
+		}
+		sc.close();
+	}
+
+}
